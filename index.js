939fd2bf03b2f3de1983b2fbeae9d88a923ea898
Viet Pham <phamviet@users.noreply.github.com>
2018-06-06 13:42:02 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { send } = require('micro');
+
+module.exports = (req, res) => {
+  let statusCode = 200;
+  const data = {};
+
+  if (!fs.existsSync('/etc/ipsec.d/cacerts/ca.cert.pem')) {
+    data.error = 'CA certificate is not found';
+    statusCode = 500;
+  } else {
+    Object.assign(data, {
+      ca: fs.readFileSync('/etc/ipsec.d/cacerts/ca.cert.pem', 'utf8'),
+      private_key: fs.readFileSync('/etc/ipsec.d/private/client.pem', 'utf8'),
+      certificate: fs.readFileSync('/etc/ipsec.d/certs/client.cert.pem', 'utf8'),
+      password: fs.readFileSync('/etc/ipsec.d/client.cert.p12.password', 'utf8')
+    });
+  }
+
+  send(res, statusCode, data)
+}
